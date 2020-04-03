65666baba4fc2a962119ae00a6e366d0e71f764b
Athan Clark <athan.clark@gmail.com>
2020-04-03 11:25:11 -0700

    instances, and to / from Array

--- src/Data/IxSet.purs
+++ src/Data/IxSet.purs
@@ -1,23 +1,46 @@
 module Data.IxSet
-  ( Index (..), IxSet
-  , new, insert, delete, lookup
+  ( Index (..), IxSet, decodeJsonIxSet
+  , new, insert, insertMany, delete, lookup, fromArray, toArray
   ) where
 
 import Prelude
 import Data.Maybe (Maybe)
+import Data.Either (Either)
+import Data.Generic.Rep (class Generic)
+import Data.Argonaut (Json, class EncodeJson, class DecodeJson, encodeJson, decodeJson)
+import Data.ArrayBuffer.Class (class EncodeArrayBuffer, class DecodeArrayBuffer)
+import Data.Vec (Vec)
+import Data.Traversable (traverse)
 import Foreign.Object (Object)
-import Foreign.Object (empty, insert, delete, lookup) as Obj
+import Foreign.Object (empty, insert, delete, lookup, toArrayWithKey) as Obj
 import Effect (Effect)
 import Effect.Ref (Ref)
 import Effect.Ref (new, modify_, read) as Ref
+import Effect.Unsafe (unsafePerformEffect)
 
 
 newtype Index = Index String
+derive instance genericIndex :: Generic Index _
+derive newtype instance eqIndex :: Eq Index
+derive newtype instance showIndex :: Show Index
+derive newtype instance encodeJsonIndex :: EncodeJson Index
+derive newtype instance decodeJsonIndex :: DecodeJson Index
+derive newtype instance encodeArrayBufferIndex :: EncodeArrayBuffer Index
+derive newtype instance decodeArrayBufferIndex :: DecodeArrayBuffer Index
 
 newtype IxSet a = IxSet
-  { genIx :: a -> Effect Index
+  { genIx   :: a -> Effect Index
   , valsRef :: Ref (Object a)
   }
+
+instance encodeJsonIxSet :: EncodeJson a => EncodeJson (IxSet a) where
+  encodeJson set =
+    encodeJson (unsafePerformEffect (toArray set))
+
+decodeJsonIxSet :: forall a. DecodeJson a => (a -> Effect Index) -> Json -> Either String (Effect {set :: IxSet a, indicies :: Array Index})
+decodeJsonIxSet getIx json = do
+  xs <- decodeJson json
+  pure (fromArray getIx xs)
 
 
 new :: forall a. (a -> Effect Index) -> Effect (IxSet a)
@@ -33,6 +56,10 @@ insert x (IxSet {genIx, valsRef}) = do
   pure ix'
 
 
+insertMany :: forall a n. Vec n a -> IxSet a -> Effect (Vec n Index)
+insertMany xs set = traverse (flip insert set) xs
+
+
 delete :: forall a. Index -> IxSet a -> Effect Unit
 delete (Index ix) (IxSet {valsRef}) =
   Ref.modify_ (Obj.delete ix) valsRef
@@ -42,3 +69,15 @@ lookup :: forall a. Index -> IxSet a -> Effect (Maybe a)
 lookup (Index ix) (IxSet {valsRef}) = do
   vals <- Ref.read valsRef
   pure (Obj.lookup ix vals)
+
+
+-- | Given some array of elements (stored as an Array), build a indexed set and report their indicies.
+fromArray :: forall a. (a -> Effect Index) -> Array a -> Effect {set :: IxSet a, indicies :: Array Index}
+fromArray getIx xs = do
+  set <- new getIx
+  indicies <- traverse (flip insert set) xs
+  pure {set, indicies}
+
+
+toArray :: forall a. IxSet a -> Effect (Array a)
+toArray (IxSet {valsRef}) = Obj.toArrayWithKey (flip const) <$> Ref.read valsRef
